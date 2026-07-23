eb528c9a0089b94b5408e75904ae752f71df603d
khalil rebhi <khalil.rebhi@itecgroupe.com>
2026-07-23 10:05:06 +0100

    Remove unused routes for devis, bl, facture, avoir, and journal

--- routes/index.js
+++ routes/index.js
@@ -2,20 +2,6 @@ const express = require("express");
 
 const router = express.Router();
 
-const devis = require("./devis");
-const bl = require("./bl");
-const facture = require("./facture");
-const avoir = require("./avoir");
-const journal = require("./journal");
-const journalClient = require("./journal.client");
-
-router.use("/api/devis", devis);
-router.use("/api/bl", bl);
-router.use("/api/facture", facture);
-router.use("/api/avoir", avoir);
-router.use("/api/journal", journal);
-router.use("/api/journal/client", journalClient);
-
 // DAF360 HR attestations (consumed by daf360-rh-service via /pdf/api/arx/*).
 const arx = require("./arx");
 const render = require("./render");
